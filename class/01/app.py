e05fffd7cb8ccb1059835d134ea24994ea59b7d0
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-07-29 03:29:43 +0500

    first commit

---
+++ class/01/app.py
@@ -0,0 +1,34 @@
+import keyword
+
+# print(keyword.kwlist)
+
+# hello world program 
+# print("hello world")
+
+# data tpyes
+
+
+# print(1_000_000_000)
+# print(123)
+# print(1e309)
+# print(1.7e309)
+# print("sharjeel ahmed")
+# print('usman is my best friend')
+# print('''
+#       i 
+#       am
+#       a 
+#       develpor
+# ''')
+
+# basic calculator using inputs
+
+# num1 = float(input("enter first number : "))
+# num2 = float(input("enter second number : "))
+# print(f"result : {num1 + num2}")
+
+# sequence data types
+# print([1 ,2, 3])
+# print(type(((1, 2, 3))))
+# print({"name" : "sharjeel" , id : 123})
+
